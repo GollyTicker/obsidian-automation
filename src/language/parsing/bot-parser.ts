05c07a1e4a49131296b964bae13d035f2cfd1f29
GollyTicker <golly.ticker@gmail.com>
2021-10-01 01:23:52 +0200

    Bot Parser Definitions. Improved naming.

--- src/language/parsing/bot-parser.ts
+++ src/language/parsing/bot-parser.ts
@@ -28,14 +28,23 @@ export type BotParser<T> = (state: St) => Parser<[T, St]>
 
 // =========== create BotParsers =============
 
+/**
+ * todo.
+ */
 export function fromSimple<T>(parser: Parser<T>): BotParser<T> {
     return (st) => parser.map(withSt(st))
 }
 
+/**
+ * todo.
+ */
 export function succeed<T>(t: T): BotParser<T> {
     return fromSimple(P.succeed(t))
 }
 
+/**
+ * todo.
+ */
 export function regexp(r: RegExp): BotParser<string> {
     return fromSimple(P.regexp(r))
 }
@@ -43,23 +52,38 @@ export function regexp(r: RegExp): BotParser<string> {
 
 // =========== work with the result or the state =============
 
+/**
+ * todo.
+ */
 export function runWith<T>(p: BotParser<T>, st: St): Parser<[T, St]> {
     return p(st)
 }
 
 
+/**
+ * todo.
+ */
 export function result<T>(parsed: [T, St]): T {
     return parsed[0]
 }
 
+/**
+ * todo.
+ */
 function getSt<T>(parsed: [T, St]): St {
     return parsed[1]
 }
 
+/**
+ * todo.
+ */
 export function withSt<T>(st: St): ((t: T) => [T, St]) {
     return (t) => [t, st]
 }
 
+/**
+ * todo.
+ */
 export function withResult<T>(t: T): ((tpl: [any, St]) => [T, St]) {
     return (tpl) => [t, getSt(tpl)]
 }
@@ -67,47 +91,80 @@ export function withResult<T>(t: T): ((tpl: [any, St]) => [T, St]) {
 
 // =========== BotParser combinators - hence B suffix =============
 
+/**
+ * todo.
+ */
 export function surroundB<T>(before: BotParser<string>, elt: BotParser<T>, after: BotParser<string>): BotParser<T> {
     return __(before, thenB, __(elt, skipB, after))
 }
 
 // hint: it's the Monad bind
+/**
+ * todo.
+ */
 export function chainB<T, R>(first: BotParser<T>, chained: (t: T) => BotParser<R>): BotParser<R> {
     return (st0) => first(st0).chain(([t, st1]) => chained(t)(st1))
 }
 
+/**
+ * todo.
+ */
 export function lazyChainB<T, R>(first: () => BotParser<T>, chained: (t: T) => BotParser<R>): BotParser<R> {
     return (st0) => first()(st0).chain(([t, st1]) => chained(t)(st1))
 }
 
+/**
+ * todo.
+ */
 export function mapB<T, R>(parser: BotParser<T>, func: (t: T) => R): BotParser<R> {
     return __(parser, chainB, (t: T) => succeed(func(t)))
 }
 
+/**
+ * todo.
+ */
 export function withResultB<T>(parser: BotParser<any>, t: T): BotParser<T> {
     return __(parser, mapB, () => t)
 }
 
-export function thenB<T>(first: BotParser<any>, second: BotParser<T>): BotParser<T> {
-    return __(first, chainB, () => second)
+/**
+ * todo.
+ */
+export function thenB<T>(skipped: BotParser<any>, parser: BotParser<T>): BotParser<T> {
+    return __(skipped, chainB, () => parser)
 }
 
-export function skipB<T>(first: BotParser<T>, second: BotParser<any>): BotParser<T> {
-    return __(first, chainB, (t: T) => withResultB(second, t))
+/**
+ * todo.
+ */
+export function skipB<T>(parser: BotParser<T>, skipped: BotParser<any>): BotParser<T> {
+    return __(parser, chainB, (t: T) => withResultB(skipped, t))
 }
 
+/**
+ * todo.
+ */
 export function altB<T>(...parsers: BotParser<T>[]): BotParser<T> {
     return (st) => P.alt<[T, St]>(...parsers.map(p => p(st)))
 }
 
+/**
+ * todo.
+ */
 export function optionalOrElse<T>(parser: BotParser<T>, fallback: T): BotParser<T> {
     return altB(parser, succeed(fallback))
 }
 
+/**
+ * todo.
+ */
 export function mapStB<T>(parser: BotParser<T>, func: (st: St) => St): BotParser<T> {
     return (st0: St) => parser(st0).map(([t, st1]) => [t, func(st1)])
 }
 
+/**
+ * todo.
+ */
 export function onSt(func: (st: St) => St): BotParser<""> {
     return __(succeed<"">(""), mapStB, func)
 }
